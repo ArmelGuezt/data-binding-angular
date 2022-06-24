c50b2976b70ad8440ad76fcc064c22096127e50e
Armel Criativo Digital <armel@iMac-de-Armel.local>
2022-06-24 11:40:34 -0300

    fist commit

---
+++ src/app/features/sell/components/products/products.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  titulo = "Produtos!"
+
+  produto = {
+    descricao: "Playstation5",
+    preco: 4500,
+    quantidade: 1
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("teste");
+  }
+
+  precoComDesconto (preco: number) {
+    return preco * 0.90;
+  }
+
+}
